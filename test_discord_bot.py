b0025d3b92436fd7c91f6ff3e3ba2c9a4791f50d
AutoPR <autopr@irgolic.com>
2023-04-25 22:32:48 +0000

    Test changes with chat and non-chat models for compatibility

--- test_discord_bot.py
+++ test_discord_bot.py
@@ -7,16 +7,16 @@ class TestDiscordBot(unittest.TestCase):
     def setUp(self):
         self.bot = DiscordBot()
 
-    def test_generate_response_with_gpt4(self):
+    def test_generate_response_with_chat_model(self):
+        with patch('openai.api_call') as mock_api_call:
+            mock_api_call.return_value = {'choices': [{'text': 'Test response.'}]}
+            response = generate_response(prompt="Test prompt.", model="gpt-4-chat")
+            self.assertEqual(response, 'Test response.')
+
+    def test_generate_response_with_non_chat_model(self):
         with patch('openai.api_call') as mock_api_call:
             mock_api_call.return_value = {'choices': [{'text': 'Test response.'}]}
             response = generate_response(prompt="Test prompt.", model="gpt-4")
-            self.assertEqual(response, 'Test response.')
-
-    def test_generate_response_with_gpt3_5_turbo(self):
-        with patch('openai.api_call') as mock_api_call:
-            mock_api_call.return_value = {'choices': [{'text': 'Test response.'}]}
-            response = generate_response(prompt="Test prompt.", model="gpt-3.5-turbo")
             self.assertEqual(response, 'Test response.')
 
 if __name__ == '__main__':
